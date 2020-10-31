a5657e9aec79bbf4bb9deded2d1a1a73cf782f77
I066841 <a.kulshreshtha@sap.com>
2020-10-31 13:11:26 +0530

    Learnt Arrow Functions

---
+++ 2-arrow-function.js
@@ -0,0 +1,29 @@
+// const square = function (num) {
+//     return num * num
+// }
+
+const { ENETDOWN } = require("constants")
+
+const square = (num) => num * num
+console.log(square(67))
+
+const event = {
+    name: 'Birthday Party',
+    guestList: ['Kanupriya', 'Abhijeet', 'Ayaan'],
+    // printGuestList: function() {
+    //     console.log('Guest List for ' + this.name)
+
+    //     this.guestList.forEach((guest) => {
+    //        console.log(guest + ' is attending ' + this.name)
+    //     })
+    // }
+    printGuestList() {
+        console.log('Guest List for ' + this.name)
+
+        this.guestList.forEach((guest) => {
+           console.log(guest + ' is attending ' + this.name)//Here this.name is accessible because it is taking reference of this from parent function printGuestList()
+        })
+    }    
+}
+
+event.printGuestList()
