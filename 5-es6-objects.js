a6f26e422a420b15df248c833a02cb24eab361c1
I066841 <a.kulshreshtha@sap.com>
2020-11-08 00:21:44 +0530

    es6 short hands

---
+++ 5-es6-objects.js
@@ -0,0 +1,11 @@
+// Shorthand syntax
+ const name = 'Abhijeet'
+ const userAge = 35
+
+ const user = {
+     name,//Shorthand syntax where the poperty name is taken from variable declared above
+     age: userAge,
+     location: 'Bangalore'
+ }
+
+ console.log(user)
